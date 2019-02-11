8fc1e47ef0c9cc2d0141c27af6f27263fd5eec42
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-11 15:40:20 +0000

    Create Glimpse testing framework with 2 matchers; isTheSameAs and hasThisInIt

--- glimpse.js
+++ glimpse.js
@@ -1,10 +1,21 @@
-function Glimpse() {
-}
-
-Glimpse.prototype.theseTwoAreTheSame = function(a, b) {
-  return !!(a === b)
-}
-
-Glimpse.prototype.thisIsInThat = function(a, array) {
-  return array.includes(a);
+var makeGlimpse = function() {
+  function glimpse(compareOne) {
+    return {
+      isTheSameAs: function(compareTwo) {
+        if (compareOne === compareTwo) {
+          console.log(`%cTest passed for ${compareOne} being the same as ${compareTwo}`, 'color: #00ba41');
+        } else {
+          console.log(`%cTest failed for ${compareOne} being the same as ${compareTwo}`, 'color: #ce0000');
+        }
+      },
+      hasThisInIt: function(compareTwo) {
+        if (compareOne.includes(compareTwo)) {
+          console.log(`%cTest passed for [${compareOne}] having ${compareTwo} inside it`, 'color: #00ba41');
+        } else {
+          console.log(`%cTest failed for [${compareOne}] having ${compareTwo} inside it`, 'color: #ce0000');
+        }
+      }
+    }
+  }
+  return glimpse;
 }
