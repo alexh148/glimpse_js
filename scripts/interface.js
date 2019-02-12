0774f78a8486ff7444bd71b2950b8990cfd1ceea
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-02-12 16:07:15 +0000

    added display array function to the page

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,9 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
   var note = new Note();
+  loadMessages();
+
+
 
   submit.addEventListener('click', function(event) {
     event.preventDefault();
     var message = document.getElementById('message').value
     note.addNote(message)
+    loadLastMessage();
   });
+
+  function loadMessages() {
+    var allMessages = note.notesList
+    for (i = 0; i < allMessages.length; i++ ){
+      text = (`<div id="links" class="rounded"> ${allMessages[i]}</div>`)
+      document.getElementById('link-container').innerHTML += text;
+    }
+  }
+
+  function loadLastMessage() {
+    console.log(note)
+  text = (`<div id="links" class="rounded"> <a href="">${note.notesList.slice(-1)[0]}</a></div>`)
+  document.getElementById('link-container').innerHTML += text;
+  }
 });
