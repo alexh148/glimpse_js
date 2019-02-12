ab4febdb0a470a5cbc40e15cbe36e6854d14ce88
Katie Cornish <katielcornish@gmail.com>
2019-02-12 15:12:30 +0000

    Add note button adds note to notes array

--- noteSpec.js
+++ noteSpec.js
@@ -2,8 +2,6 @@ var note = new Note();
 
 describe("#notesList", function() {
   it("Should have a list of notes", function() {
-    var note = new Nosdte();
-
     glimpse(note.notesList).hasThisInIt("Hello World");
   })
 })
