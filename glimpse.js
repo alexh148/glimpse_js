e88b7e8d6645f31add00d44f586e3afac1349a62
Paul <paul.ayling@bglgroup.co.uk>
2019-02-12 09:30:01 +0000

    Describe and It methods, HTML correctly displaying

--- glimpse.js
+++ glimpse.js
@@ -1,18 +1,32 @@
+const describe = function(description, nextStep) {
+  console.log(description)
+  document.getElementById('tests').innerHTML += `<span style='color: brown; font-size: 25px;'><br>${description}<br>`;
+  nextStep()
+}
+
+const it = function(description, nextStep) {
+  console.log(description)
+  document.getElementById('tests').innerHTML += `<span style='color: black;'><br>${description}<br>`;
+  nextStep()
+}
+
 function makeGlimpse() {
   function glimpse(compareOne) {
     return {
       isTheSameAs: function(compareTwo) {
         if (compareOne === compareTwo) {
           console.log(`%cTest passed for ${compareOne} being the same as ${compareTwo}`, 'color: #00ba41');
+          document.getElementById('tests').innerHTML += `<span style='color: #00ba41;'>Test passed for ${compareOne} being the same as ${compareTwo}<br>`, 'color: #00ba41';
         } else {
           console.log(`%cTest failed for ${compareOne} being the same as ${compareTwo}`, 'color: #ce0000');
+          document.getElementById('tests').innerHTML += `<span style='color: #ce0000;'>Test failed for ${compareOne} being the same as ${compareTwo}<br>`;
         }
       },
       hasThisInIt: function(compareTwo) {
         if (compareOne.includes(compareTwo)) {
-          console.log(`%cTest passed for [${compareOne}] having ${compareTwo} inside it`, 'color: #00ba41');
+          document.getElementById('tests').innerHTML += `<span style='color: #00ba41;'>Test passed for [${compareOne}] having ${compareTwo} inside it<br>`;
         } else {
-          console.log(`%cTest failed for [${compareOne}] having ${compareTwo} inside it`, 'color: #ce0000');
+          document.getElementById('tests').innerHTML += `<span style='color: #ce0000;'>Test failed for [${compareOne}] having ${compareTwo} inside it<br>`;
         }
       }
     }
--- noteSpec.js
+++ noteSpec.js
@@ -1,5 +1,24 @@
-glimpse = new makeGlimpse();
-glimpse(5).isTheSameAs(6);
-glimpse(5).isTheSameAs(5);
-glimpse([3,4,5,6]).hasThisInIt(5);
-glimpse([3,4,5,6]).hasThisInIt(10);
+
+var glimpse = new makeGlimpse()
+
+var five = 5
+var six = 6
+var arr = [1,2,3,4,5]
+
+describe("#equals", function() {
+  it("Five should equal 5", function() {
+    glimpse(five).isTheSameAs(5);
+  })
+  it("Should return true", function() {
+    glimpse(five).isTheSameAs(6);
+  })
+})
+
+describe("#includes", function() {
+  it("Array contains 5", function() {
+    glimpse(arr).hasThisInIt(5);
+  })
+  it("Array contains 6", function() {
+    glimpse(arr).hasThisInIt(6);
+  })
+})
